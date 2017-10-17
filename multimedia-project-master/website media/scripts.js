2727c5d5a61f4e59bed42f537fc4ab76c5b0c79b
Timothy Aitchison <Timothy.Aitchison@protonmail.com>
2017-10-17 21:36:57 +1300

    Add files via upload

---
+++ multimedia-project-master/website media/scripts.js
@@ -0,0 +1,38 @@
+/*
+ *	Dropdown menu
+ */
+
+function menuClick() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.menuIcon')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/*
+ *	Slideshow on homepage
+ */
+
+function carousel() {
+	var myIndex = 0;
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
